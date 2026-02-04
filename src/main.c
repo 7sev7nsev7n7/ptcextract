fcf920c8030dee5cf7c7016a34850d00d56e236e
7sev7nsev7n7 <7sev7nsev7n7@gmail.com>
2026-02-04 02:06:42 -0600

    Completely refactored argument handling via getopt, allowing easier implementation of new command line arguments if necessary

--- src/main.c
+++ src/main.c
@@ -12,116 +12,141 @@
 #define PONY_VERSION "3ca61ba6041902" // pony version upon which tool was based upon
 #define PONY_TOWN_VERSION "v0.124.0" // pony town version upon which tool was based upon
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 2
+#define VERSION_MINOR 3
 #define VERSION_HOTFIX 0
 
-void print_title(void); // print intro title with licensing and versioning info
+void print_title(); // print intro title with licensing and versioning info
+void print_usage(char*); // print tool usage with options
 
 int main(int argc, char *argv[]) {
-  print_title(); // print opening title
-
   // argument checking
-  if (argc<2) {
-    fprintf(stderr, "no files specified\n");
-    return 1;
-  } 
+  if (argc==1) { // print usage if no arguments are supplied
+        print_usage(argv[0]);
+        return 1;
+  }
 
-  // process every argument as an input file, this might change in the future
-  for (int mainloop=1; mainloop<argc; mainloop++) {
-    // opening file and reading contents
-    int file = open(argv[mainloop], O_RDONLY);
+  int opt, notitle=0;
+  char filename[512]=""; // only supports one file at a time
+
+  while ((opt = getopt(argc, argv, "qhf:i:")) != - 1) {
+    switch(opt) {
+      case 'q': // quiet (no title)
+        notitle=1;
+        break;
+      case 'i': // input file
+        strcpy(filename, optarg);
+        break;
+      case 'f': // multiple pony file (i.e. "Export All" function)
+        fprintf(stderr, "Unpacking \"Export All\" files currently unsupported\n");
+        return 1;
+      case 'h':
+      default: // 
+        print_usage(argv[0]);
+        return 1;
+    }
+  }
+
+  if (notitle==0) // print title if quiet flag not set
+    print_title();
+
+  // opening file and reading contents
+  int file;
+  if (filename=="") {
+    fprintf(stderr, "no input file specified\n");
+    return 1;
+  } else {
+    file = open(filename, O_RDONLY);
     if (file==-1) {
       fprintf(stderr, "file not found\n");
       return 1;
     }
-
-    // obtain filesize
-    int filesize=fsize(file);
-    if (filesize<=0) {
-      fprintf(stderr, "file is empty, quitting\n");
-      return 1;
-    }
-
-    // read file
-    char* raw_string = malloc(filesize);
-    read(file, raw_string, filesize);
-    // create uint8_t array for decoding
-    uint8_t base64_decoded[strlen(raw_string)];
-    // base64 decode string
-    int decoded_length=decode(raw_string, base64_decoded);
-    close(file); // close file as it is no longer required
-    free(raw_string); // free raw string as it is no longer required, and could eventually cause memory leak
-
-    /* ---------- BEGIN UGLY DEBUG CODE ---------- */
-    /* ---------- BEGIN UGLY DEBUG CODE ---------- */
-
-    // print newline if we're processing more than one file
-    if(mainloop>1) printf("\n");
-    printf("-- Processing file: %s\n", argv[mainloop]);
-
-    // debug print version
-    printf("-- Pony version: ");
-    for (int i=0; i<7; i++)
-      printf("%.2x", *(base64_decoded+i));
-    printf("\n\n");
-
-    // debug print character name
-    // character name is always after the first nine bytes
-    int character_name_length = (int)*(base64_decoded+8);
-    printf("-- Character name: ");
-    for (int i=1; i<character_name_length; i++) {
-      printf("%c", *(base64_decoded+i+8));
-    }
-    printf("\n");
-
-    // debug get byte count until end of file (or start of description if applicable)
-    int remaining_byte_count = *(base64_decoded+character_name_length+8);
-    if (remaining_byte_count>0x80) { // shitty necessary thing because of reasons
-      remaining_byte_count = (*(base64_decoded+character_name_length+8)-0x80) + (*(base64_decoded+character_name_length+9)*0x80) + 1;
-    }
-
-    // debug print character description
-    if (int character_description_length = decoded_length-(remaining_byte_count+character_name_length+9); character_description_length>0) {
-      printf("-- Character description: ");
-      for (int i=0; i<character_description_length; i++) {
-        printf("%c", *(base64_decoded+character_name_length+remaining_byte_count+i+9));
-      }
-      printf("\n");
-    }
-    printf("\n");
-
-    // debug get color start position and count
-    int color_start_position=0;
-    int color_count=0;
-    for (int i=(character_name_length+9); i<remaining_byte_count; i++) {
-      if (*(base64_decoded+i)==0x64) {
-        color_start_position=i+1;
-        color_count=(int)*(base64_decoded+i+1)*3;
-        break;
-      }
-    }
-
-    // debug print color count and list colors
-    printf("-- Color count: %d (using %d bytes)\n", color_count/3, color_count);
-    printf("-- Color hex codes:\n");
-    for (int i=0; i<color_count; i++) {
-      if (i%3==0) printf("#");
-      printf("%.2x", *(base64_decoded+color_start_position+i+1));
-      if (i%3==2) printf("\n");
-    }
-    printf("\n");
-
-    // debug print rest of values as hex
-    int rest_hex_start = color_start_position+color_count+1;
-    int rest_hex_end=0;
-    printf("-- Printing rest of values as uint8 hex values (starting at index %d or 0x%.2x): \n", rest_hex_start, rest_hex_start);
-    for (int i=1; i<decoded_length-rest_hex_start+1; i++) {
-      rest_hex_end++;
-      printf("%.2x ", *(base64_decoded+i+rest_hex_start-1));
-      if (i%16==0 && i+1!=decoded_length-rest_hex_start+1) printf("\n");
-    }
-    printf("\n-- Total count of rest of hex codes: %d\n",rest_hex_end);
   }
+
+  // obtain filesize
+  int filesize=fsize(file);
+  if (filesize<=0) {
+    fprintf(stderr, "file is empty, quitting\n");
+    return 1;
+  }
+
+  // read file
+  char* raw_string = malloc(filesize);
+  read(file, raw_string, filesize);
+  // create uint8_t array for decoding
+  uint8_t base64_decoded[strlen(raw_string)];
+  // base64 decode string
+  int decoded_length=decode(raw_string, base64_decoded);
+  close(file); // close file as it is no longer required
+  free(raw_string); // free raw string as it is no longer required, and could eventually cause memory leak
+
+  /* ---------- BEGIN UGLY DEBUG CODE ---------- */
+  /* ---------- BEGIN UGLY DEBUG CODE ---------- */
+
+  // print newline if we're processing more than one file
+  printf("-- Processing file: %s\n", filename);
+
+  // debug print version
+  printf("-- Pony version: ");
+  for (int i=0; i<7; i++)
+    printf("%.2x", *(base64_decoded+i));
+  printf("\n\n");
+
+  // debug print character name
+  // character name is always after the first nine bytes
+  int character_name_length = (int)*(base64_decoded+8);
+  printf("-- Character name: ");
+  for (int i=1; i<character_name_length; i++) {
+    printf("%c", *(base64_decoded+i+8));
+  }
+  printf("\n");
+
+  // debug get byte count until end of file (or start of description if applicable)
+  int remaining_byte_count = *(base64_decoded+character_name_length+8);
+  if (remaining_byte_count>0x80) { // shitty necessary thing because of reasons
+    remaining_byte_count = (*(base64_decoded+character_name_length+8)-0x80) + (*(base64_decoded+character_name_length+9)*0x80) + 1;
+  }
+
+  // debug print character description
+  if (int character_description_length = decoded_length-(remaining_byte_count+character_name_length+9); character_description_length>0) {
+    printf("-- Character description: ");
+    for (int i=0; i<character_description_length; i++) {
+      printf("%c", *(base64_decoded+character_name_length+remaining_byte_count+i+9));
+    }
+    printf("\n");
+  }
+  printf("\n");
+
+  // debug get color start position and count
+  int color_start_position=0;
+  int color_count=0;
+  for (int i=(character_name_length+9); i<remaining_byte_count; i++) {
+    if (*(base64_decoded+i)==0x64) {
+      color_start_position=i+1;
+      color_count=(int)*(base64_decoded+i+1)*3;
+      break;
+    }
+  }
+
+  // debug print color count and list colors
+  printf("-- Color count: %d (using %d bytes)\n", color_count/3, color_count);
+  printf("-- Color hex codes:\n");
+  for (int i=0; i<color_count; i++) {
+    if (i%3==0) printf("#");
+    printf("%.2x", *(base64_decoded+color_start_position+i+1));
+    if (i%3==2) printf("\n");
+  }
+  printf("\n");
+
+  // debug print rest of values as hex
+  int rest_hex_start = color_start_position+color_count+1;
+  int rest_hex_end=0;
+  printf("-- Printing rest of values as uint8 hex values (starting at index %d or 0x%.2x): \n", rest_hex_start, rest_hex_start);
+  for (int i=1; i<decoded_length-rest_hex_start+1; i++) {
+    rest_hex_end++;
+    printf("%.2x ", *(base64_decoded+i+rest_hex_start-1));
+    if (i%16==0 && i+1!=decoded_length-rest_hex_start+1) printf("\n");
+  }
+  printf("\n-- Total count of rest of hex codes: %d\n",rest_hex_end);
   return 0;
 }
 
@@ -131,4 +156,14 @@ void print_title() {
   printf("For license details, please visit GitHub repository: https://github.com/7sev7nsev7n7/ptcextract\n");
   printf("Based on pony version %s\n", PONY_VERSION);
   printf("Based on Pony Town %s\n\n", PONY_TOWN_VERSION);
+}
+
+void print_usage(char* execpath) {
+  fprintf(stderr, "Usage: %s [-h] [[-f \"file\"] | [-i \"file\"]]\n\n", execpath);
+  fprintf(stderr, "ptcextract - extract .ptc character information\n\n");
+  fprintf(stderr, "options:\n");
+  fprintf(stderr, "  -h                 show this help message and exit\n");
+  fprintf(stderr, "  -i \"file\"          specify single character input file\n");
+  fprintf(stderr, "  -f \"file\"          specify multiple character input file (pending proper implementation)\n");
+  fprintf(stderr, "  -q                 do not print welcome title\n");
 }
