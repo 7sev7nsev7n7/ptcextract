1728e7df218191caeb056711941922c3aaa6ae79
7sev7nsev7n7 <7sev7nsev7n7@gmail.com>
2026-02-12 23:51:29 -0600

    Moved function definitions to libpony.h for better organization. Reorganized preproc definitions to work with rearrangement

--- src/lib/libpony.h
+++ src/lib/libpony.h
@@ -45,3 +45,20 @@ int decode(const char* input, uint8_t* output) {
 
   return count;
 }
+
+void print_title() {
+  printf("Extract .ptc character information (v%d.%d.%d)\n", VERSION_MAJOR, VERSION_MINOR, VERSION_HOTFIX);
+  printf("Ptcextract is open software\n");
+  printf("For license details, please visit GitHub repository: https://github.com/7sev7nsev7n7/ptcextract\n");
+  printf("Based on pony version %s\n", PONY_VERSION);
+  printf("Based on Pony Town %s\n\n", PONY_TOWN_VERSION);
+}
+
+void print_usage(char* execpath) {
+  fprintf(stderr, "Usage: %s [-q] [-h] file(s)\n\n", execpath);
+  fprintf(stderr, "ptcextract - extract .ptc character information\n\n");
+  fprintf(stderr, "options:\n");
+  fprintf(stderr, "  -h                 show this help message and exit\n");
+  fprintf(stderr, "  -q                 do not print welcome title\n");
+  fprintf(stderr, "  -x                 print remaining hex values\n");
+}
--- src/main.c
+++ src/main.c
@@ -1,3 +1,9 @@
+#define PONY_VERSION "3ca61ba6041902" // pony version upon which tool was based upon
+#define PONY_TOWN_VERSION "v0.124.0" // pony town version upon which tool was based upon
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 4
+#define VERSION_HOTFIX 6
+
 #include <b64/cdecode.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -9,15 +15,6 @@
 #include <unistd.h>
 
 #include "lib/libpony.h"
-
-#define PONY_VERSION "3ca61ba6041902" // pony version upon which tool was based upon
-#define PONY_TOWN_VERSION "v0.124.0" // pony town version upon which tool was based upon
-#define VERSION_MAJOR 1
-#define VERSION_MINOR 4
-#define VERSION_HOTFIX 6
-
-void print_title(); // print intro title with licensing and versioning info
-void print_usage(char*); // print tool usage with options
 
 int main(int argc, char *argv[]) {
   // argument handling
@@ -173,21 +170,4 @@ int main(int argc, char *argv[]) {
     }
   }
   exit(0);
-}
-
-void print_title() {
-  printf("Extract .ptc character information (v%d.%d.%d)\n", VERSION_MAJOR, VERSION_MINOR, VERSION_HOTFIX);
-  printf("Ptcextract is open software\n");
-  printf("For license details, please visit GitHub repository: https://github.com/7sev7nsev7n7/ptcextract\n");
-  printf("Based on pony version %s\n", PONY_VERSION);
-  printf("Based on Pony Town %s\n\n", PONY_TOWN_VERSION);
-}
-
-void print_usage(char* execpath) {
-  fprintf(stderr, "Usage: %s [-q] [-h] file(s)\n\n", execpath);
-  fprintf(stderr, "ptcextract - extract .ptc character information\n\n");
-  fprintf(stderr, "options:\n");
-  fprintf(stderr, "  -h                 show this help message and exit\n");
-  fprintf(stderr, "  -q                 do not print welcome title\n");
-  fprintf(stderr, "  -x                 print remaining hex values\n");
 }
