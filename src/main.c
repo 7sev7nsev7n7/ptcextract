7ad24fbb0e5d480e5c785fc57ced0df62b09aae4
7sev7nsev7n7 <7sev7nsev7n7@gmail.com>
2026-02-13 16:50:35 -0600

    Bump minor version to signify finalization of current implementation of multiple option and file handling

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 #define PONY_VERSION "3CA61BA6041902" // pony version upon which tool was based upon
 #define PONY_TOWN_VERSION "v0.124.0" // pony town version upon which tool was based upon
 #define VERSION_MAJOR 2
-#define VERSION_MINOR 1
-#define VERSION_HOTFIX 10
+#define VERSION_MINOR 2
+#define VERSION_HOTFIX 0
 
 #include <b64/cdecode.h>
 #include <errno.h>
