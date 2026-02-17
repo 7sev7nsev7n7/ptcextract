2bf6dcfd77fd52301eeb65815670904d7efd42a0
7sev7nsev7n7 <7sev7nsev7n7@gmail.com>
2026-02-16 22:01:45 -0600

    Changed printing hex values to individual lines, to make debugging easier

--- src/main.c
+++ src/main.c
@@ -186,8 +186,7 @@ int main(int argc, char* argv[]) {
       printf("Printing rest of values as uint8 hex values (starting at index %d or 0x%.2X): \n", rest_hex_start, rest_hex_start);
       for (int i=1; i<decoded_length-rest_hex_start+1; i++) {
         rest_hex_end++;
-        printf("%.2X ", *(base64_decoded+i+rest_hex_start-1));
-        if (i%16==0 && i+1!=decoded_length-rest_hex_start+1) printf("\n");
+        printf("%.2X\n", *(base64_decoded+i+rest_hex_start-1));
       }
       printf("\n-- Total count of rest of hex codes: %d\n",rest_hex_end);
     }
